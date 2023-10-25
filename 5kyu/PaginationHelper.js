4b02ec5e043311776274e23e3f7c050b3b2c0562
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-10-25 17:46:40 -0300

    Update PaginationHelper.js

---
+++ 5kyu/PaginationHelper.js
@@ -0,0 +1,25 @@
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+    this.items = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+  itemCount() {
+    // returns the number of items within the entire collection
+    return this.items.length;
+  }
+  pageCount() {
+    // returns the number of pages
+    const pages = this.items.length / this.itemsPerPage;
+    return Math.ceil(pages);
+  }
+  pageItemCount(pageIndex) {
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+  }
+  pageIndex(itemIndex) {
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+  }
+}
