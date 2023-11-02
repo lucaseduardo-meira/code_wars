11c18f2de061c350d585c3fa1c799643fee01435
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-11-01 21:12:13 -0300

    Update Strip Comments.js

--- 4kyu/Strip Comments.js
+++ 4kyu/Strip Comments.js
@@ -18,3 +18,12 @@ function solution(input, markers) {
 
   return newString;
 }
+
+// function solution(input, markers) {
+//   return input
+//     .split("\n")
+//     .map((line) =>
+//       markers.reduce((line, marker) => line.split(marker)[0].trim(), line)
+//     )
+//     .join("\n");
+// }
