d5219e86118efd0319556c80ee6398f332679117
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-11-04 17:46:34 -0300

    Update Nesting Structure Comparison.js

--- 4kyu/Nesting Structure Comparison.js
+++ 4kyu/Nesting Structure Comparison.js
@@ -1,8 +1,21 @@
-function checkOne(array) {
-  array.forEach((item) => {
-    if (typeof item === "object") checkOne(item);
-    else console.log(item);
-  });
-}
+Array.prototype.sameStructureAs = function (other) {
+  if (!Array.isArray(other)) return false;
 
-checkOne([1, 2, [3, [4, [5, 6], 7], 8]]);
+  var response = true;
+
+  function checkArray(array, other) {
+    if (array.length !== other.length) response = false;
+    console.log(array, other);
+    var i = 0;
+    array.forEach((item) => {
+      if (typeof item === "object" && typeof (other[i] === "object"))
+        checkArray(item, other[i]);
+      if (typeof other[i] === "object" && typeof item !== "object")
+        response = false;
+      i++;
+    });
+  }
+
+  checkArray(this, other);
+  return response;
+};
