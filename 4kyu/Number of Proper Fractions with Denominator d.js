cc81d2b054a2235899521f9573322efcbe6754b1
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-11-05 15:48:53 -0300

    Update Number of Proper Fractions with Denominator d.js

--- 4kyu/Number of Proper Fractions with Denominator d.js
+++ 4kyu/Number of Proper Fractions with Denominator d.js
@@ -1,20 +1,14 @@
 function properFractions(n) {
   if (n <= 1) return 0;
-  const divisible = [];
+  const divisible = [n];
   const notDivisible = [1];
-  for (var i = 2; i <= n; i++) {
-    if (n % i === 0) divisible.push(i);
-    else if (n % i !== 0) {
-      var push = true;
-      for (var j = 0; j <= divisible.length; j++) {
-        if (i % divisible[j] === 0) {
-          push = false;
-          divisible.push(i);
-          break;
-        }
+  for (var i = 2; i < n; i++) {
+    for (var j = 0; j <= divisible.length; j++) {
+      if (divisible[j] % i === 0 || i % divisible[j] === 0) {
+        divisible.push(i);
+        break;
       }
-      if (push === true) notDivisible.push(i);
     }
   }
-  return notDivisible.length;
+  return n - divisible.length;
 }
