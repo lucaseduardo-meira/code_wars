09c752ff14b1016d0750177983d09ef1e994eca7
lucaseduardo-meira <102125807+lucaseduardo-meira@users.noreply.github.com>
2024-01-15 18:09:15 -0300

    Update Strip Comments.js

--- 4kyu/Strip Comments.js
+++ 4kyu/Strip Comments.js
@@ -18,12 +18,3 @@ function solution(input, markers) {
 
   return newString;
 }
-
-// function solution(input, markers) {
-//   return input
-//     .split("\n")
-//     .map((line) =>
-//       markers.reduce((line, marker) => line.split(marker)[0].trim(), line)
-//     )
-//     .join("\n");
-// }
