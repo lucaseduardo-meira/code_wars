d1ae3ee2c89026e3e67e7b5590baa73d8a41d7ca
lucaseduardo-meira <102125807+lucaseduardo-meira@users.noreply.github.com>
2024-01-23 18:09:23 -0300

    Update Create Phone Number.js

--- 6kyu/Create Phone Number.js
+++ 6kyu/Create Phone Number.js
@@ -1,6 +1,5 @@
 function createPhoneNumber(numbers) {
-  const phone = `(${numbers.slice(0, 3).join("")}) ${numbers
+  return `(${numbers.slice(0, 3).join("")}) ${numbers
     .slice(3, 6)
-    .join("")}-${numbers.slice(6, 10).join("")}`;
-  return phone;
+    .join("")}-${numbers.slice(6).join("")}`;
 }
